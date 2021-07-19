6e3cc29a2da7f20513f113bc933d56ab8aa0bf18
Paul C <dev.pcnova@gmail.com>
2021-07-19 17:48:13 -0600

    feat(login): ensured state is cleared on logout.

--- application/src/components/login/login.js
+++ application/src/components/login/login.js
@@ -3,13 +3,14 @@ import LoginForm from './login-form/loginForm';
 import './login.css';
 
 class Login extends Component {
-  
   render() {
     return (
       <div className="main-body">
         <h1 className="text-center">Login Screen</h1>
         <div className="d-flex justify-content-center mt-5">
-          <LoginForm onLogin={() => {this.props.history.push('/view-orders')}}/>
+          <LoginForm
+            onLogin={() => { this.props.history.push('/view-orders') }}
+            navState={this.props.location.state} />
         </div>
       </div>
     )
